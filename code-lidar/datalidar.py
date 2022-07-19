432e17c01c2a25537a5d3e05fdf00172cd8d5935
Ashanth <ashanth92@gmail.com>
2022-07-19 11:24:18 +0200

    modify datalidar.py file

--- code-lidar/datalidar.py
+++ code-lidar/datalidar.py
@@ -1,36 +1,48 @@
-import sys
-import numpy as np
-import os
+import sys, numpy as np, os, time, math, mathplotlib
 from rplidar import RPLidar
 
 
-#PORT_NAME = '/dev/ttyUSB0'
-PORT_NAME = '/dev/tty.usbserial-0001'
+PORT_NAME = 'COM5'
+#PORT_NAME = '/dev/tty.usbserial-0001'
 
-def run(fileName):
+def run(fileNames):
     '''Main function'''
     lidar = RPLidar(PORT_NAME)
     data = []
+    dataPos = []
     try:
         print('Recording measurments... Press Crl+C to stop.')
-        for scan in lidar.iter_scans():
-            data.append(np.array(scan))
+        for scan in lidar.iter_measurments():
+            if(scan[1]==15):
+                teta = math.radians(scan[-2])
+                x = scan[-1] * math.cos(teta)
+                y = scan[-1] * math.sin(teta)
+                data.append([scan[-2],scan[-1]])
+                dataPos.append([x,y])
+            if ( len(data) > 300 ):
+                break
             #create(fileName, data)
             #print(data)
+
     except KeyboardInterrupt:
         print('Stoping.')
     lidar.stop()
     lidar.disconnect()
+    print(data)
     #np.save(path, np.array(data))
-    create(fileName, data)
+    create(fileNames[0], data)
+    create(fileNames[1], dataPos)
 
 def create(fileName, data):
     file = open(fileName, 'w')
     print("I save my data ")
-    file.write(str(data))
+    for elm in data:
+        file.write(str(elm[0])+" , "+str(elm[1]))
+        file.write("\n")
     file.close()
 
 
 if __name__ == '__main__':
-    fileName ="data-lidar.txt"
-    run('lidar_data.txt')
+    fileName1 = "data-lidar.csv"
+    fileName2 = "data-position.csv"
+    run([fileName1,fileName2])
