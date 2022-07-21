69f7f7ff5c0b1a74494c33895b1d5f3b7b8446f9
BLADINE <59175711+BLADINE@users.noreply.github.com>
2022-07-21 11:48:02 +0200

    remove unnecessary files

--- code-lidar/datalidar.py
+++ code-lidar/datalidar.py
@@ -1,11 +1,18 @@
-import sys, numpy as np, os, time, math,
+import sys
+import numpy as np
+import os
+import csv
+import time
+import math
+import matplotlib.pyplot as plt
+import re
 from rplidar import RPLidar
 
 
-PORT_NAME = 'COM5'
-#PORT_NAME = '/dev/tty.usbserial-0001'
+#PORT_NAME = 'COM5'
+PORT_NAME = '/dev/tty.usbserial-0001'
 
-def run(fileNames):
+def run(fileNames, npoints):
     '''Main function'''
     lidar = RPLidar(PORT_NAME)
     data = []
@@ -15,11 +22,12 @@ def run(fileNames):
         for scan in lidar.iter_measurments():
             if(scan[1]==15):
                 teta = math.radians(scan[-2])
-                x = scan[-1] * math.cos(teta)
-                y = scan[-1] * math.sin(teta)
-                data.append([scan[-2],scan[-1]])
-                dataPos.append([x,y])
-            if ( len(data) > 300 ):
+                if position_filter(scan):
+                    x = scan[-1] * math.cos(teta)
+                    y = scan[-1] * math.sin(teta)
+                    data.append([scan[-2],scan[-1]])
+                    dataPos.append([x,y])
+            if ( len(data) > npoints ):
                 break
             #create(fileName, data)
             #print(data)
@@ -30,6 +38,7 @@ def run(fileNames):
     #np.save(path, np.array(data))
     create(fileNames[0], data)
     create(fileNames[1], dataPos)
+    gui(fileNames[1])
 
 def create(fileName, data):
     file = open(fileName, 'w')
@@ -39,8 +48,39 @@ def create(fileName, data):
         file.write("\n")
     file.close()
 
+def gui(filename):
+    x = []
+    y = []
+    with open(filename, newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in spamreader:
+            x.append(float((row[0].strip())))
+            y.append(float((row[1].strip())))
+    # print(x)
+    # print(y)
+    plt.plot(x, y, ".")
+    plt.show()
+
+
+
+
+
+def position_filter(scan):
+    #if direction == "avant":
+    teta = math.radians(scan[-2])
+    if (teta > 0.) and (teta < math.pi/6) :
+    # x = scan[-1] * math.cos(teta)
+    # y = scan[-1] * math.sin(teta)
+        return True
+    return False
+        
+
+
 
 if __name__ == '__main__':
     fileName1 = "data-lidar.csv"
     fileName2 = "data-position.csv"
-    run([fileName1,fileName2])
+    npoints = 100
+    #run([fileName1,fileName2], npoints)
+    print(math.radians(180))
+    print(math.pi/6.)
