8143dfb8eec598201863e0893af48af2d366c983
NgamyGianni <spyker97160@hotmail.fr>
2022-07-21 14:18:06 +0200

    add main

---
+++ code-lidar/main.py
@@ -0,0 +1,47 @@
+import a_star, subarea, time
+
+def tradMove(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
+    
+    dx = x2 - x1
+    dy = y2 - y1
+    
+    if dx != 0:
+        if dx == 1:
+            return "d"
+        else:
+            return "q"
+    else:
+        if dy == 1:
+            return "z"
+        else:
+            return "s"
+
+moves = [(5, 5), (6, 5), (7, 5), (8, 5)]
+
+commands = [tradMove(moves[i], moves[i+1]) for i in range(len(moves)-1)]
+
+print(commands)
+
+"""ser = serial.Serial('/dev/ttyACM0')
+
+for i in range(len(commands)):
+    time.sleep(0.2)
+    ser.write(commands[i].encode())"""
+
+if __name__ == "__main__":
+    #call datalidar
+    
+    long_rb = 1
+    larg_rb = 1
+    gap = 0.
+    larg_table = 10
+    point_arr = [6, 5]
+    ind_rb_i = int(larg_table / ( 2 * (larg_rb + 2. * gap) ) )
+    index_rb = [ind_rb_i,0]
+    #points = read_file("data-position.csv")
+    filename = "data-position.csv"
+    width, height, matrix = subarea.init_matrix_subdivision(
+        long_rb, larg_rb, gap, larg_table, point_arr)
+    subarea.update_matrix_subdivision(filename, width, height, index_rb, matrix)
